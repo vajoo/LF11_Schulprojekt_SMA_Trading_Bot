8491e39cd1cb1179dfaf4a6ed07ad3a3340fa726
vajoo <laurin.windhaus@gmail.com>
2023-04-04 12:13:17 +0200

    added function to store pandas dataframe into sqlite db and add function to get sql data

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 import datetime
 import sqlite3
 
-def get_1m_historical_data(ticker):
+def get_1m_historical_price_data(ticker):
     interval = '1m'
     period = '7d'
     hist_data = yf.download(ticker, interval=interval, period=period, rounding=True)
@@ -19,12 +19,26 @@ def get_yahoo_finance_live_price_from_ticker(ticker):
     price = soup.find("fin-streamer", class_="Fw(b) Fz(36px) Mb(-4px) D(ib)").text
     return price
 
-def safe_stock_price_into_db(db_file, price):
+def store_historical_price_data_to_db(db_file, ticker, historical_price_data):
+    conn = sqlite3.connect(db_file)
+    historical_price_data.to_sql(ticker, conn, if_exists='replace', index=False)
+    conn.commit()
+    conn.close()
+
+def safe_stock_price_into_db(db_file, ticker, price):
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     conn = sqlite3.connect(db_file)
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS stock_prices (symbol TEXT, price REAL, timestamp DATE)")
-    cur.execute("INSERT INTO stock_prices (symbol, price, timestamp) VALUES (?, ?, ?)", ("MSFT", float(price), timestamp))
+    cur.execute("INSERT INTO stock_prices (symbol, price, timestamp) VALUES (?, ?, ?)", (ticker, float(price), timestamp))
+    conn.commit()
+    conn.close()
+
+def get_ticker_prices_from_db(db_file, ticker):
+    conn = sqlite3.connect(db_file)
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM {}".format(ticker))
+    print(cur.fetchall())
     conn.commit()
     conn.close()
 
@@ -34,3 +48,8 @@ def clear_db(db_file, table_name):
     cur.execute("DELETE FROM "+table_name+" WHERE 1=1")
     conn.commit()
     conn.close()
+
+ticker = "MSFT"
+data = get_1m_historical_price_data(ticker)
+store_historical_price_data_to_db("test.db", ticker, data)
+get_ticker_prices_from_db("test.db", ticker)
