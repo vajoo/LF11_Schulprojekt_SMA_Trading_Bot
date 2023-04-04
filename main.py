6b14d0c3af5bf836315beedcd4a93d38739c9c9b
vajoo <laurin.windhaus@gmail.com>
2023-04-04 11:37:03 +0200

    added function to get live prices from yahoo finance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import yfinance as yf
+import requests
+from bs4 import BeautifulSoup
 
 def get_1m_historical_data(ticker):
     interval = '1m'
@@ -8,4 +10,11 @@ def get_1m_historical_data(ticker):
     hist_data['Datetime'] = hist_data['Datetime'].dt.tz_convert('Europe/Berlin').dt.tz_localize(None)
     return hist_data
 
-print(get_1m_historical_data("MSFT"))
+def get_yahoo_finance_live_price_from_ticker(ticker): 
+    url = f"https://finance.yahoo.com/quote/{ticker}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    price = soup.find("fin-streamer", class_="Fw(b) Fz(36px) Mb(-4px) D(ib)").text
+    return price
+
+print(get_yahoo_finance_live_price_from_ticker("AAPL"))
