0fd79eabbc41aa9f8fcc005298ed254e7d9ea573
vajoo <laurin.windhaus@gmail.com>
2023-04-04 11:37:58 +0200

    added function to store stock price with timestamp in database

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 import requests
 from bs4 import BeautifulSoup
+import datetime
+import sqlite3
 
 def get_1m_historical_data(ticker):
     interval = '1m'
@@ -17,4 +19,11 @@ def get_yahoo_finance_live_price_from_ticker(ticker):
     price = soup.find("fin-streamer", class_="Fw(b) Fz(36px) Mb(-4px) D(ib)").text
     return price
 
-print(get_yahoo_finance_live_price_from_ticker("AAPL"))
+def safe_stock_price_into_db(db_file, price):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    conn = sqlite3.connect(db_file)
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS stock_prices (symbol TEXT, price REAL, timestamp DATE)")
+    cur.execute("INSERT INTO stock_prices (symbol, price, timestamp) VALUES (?, ?, ?)", ("MSFT", float(price), timestamp))
+    conn.commit()
+    conn.close()
